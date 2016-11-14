e3953d98cafd66c752f2fe4fa892609cf354bbd6
James Rhoten <rhotenm@g.cofc.edu>
2016-11-14 09:24:45 -0500

    Update runAllTests.sh

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,1 +1,17 @@
-..
+#!/bin/bash
+
+#delete previous reports
+cd /temp
+rm reports.txt
+rm reports.html
+cd..
+
+#run all test cases
+
+#compare test output to oracles and create report
+/scripts/compare.py output.txt oracle.txt report.txt
+
+#change report.txt to report.html
+
+#launch reports.html 
+x-www-browser reports.html
