47c4dac7a5e1eb6675084ea76a18f097439921d1
James Rhoten <rhotenm@g.cofc.edu>
2016-11-15 07:01:23 -0500

    Update runAllTests.sh

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Delete Report.txt & Report.html
+rm Report.txt
+rm Report.html
+
 #Compile java code
 javac MainDriver.java
 javac ContainsDriver.java
@@ -7,10 +11,26 @@ javac StringUtil.java
 
 #run all test cases
 java MainDriver
+
 #compare test output to oracles and create report
-./compare.py output.txt oracle.txt report.txt
+./compare.py Results.txt Oracle.txt Report.txt
 
 #change report.txt to report.html
+echo "<!DOCTYPE html>" > Report.html
+echo "<html>" >> Report.html
+echo "<body>" >> Report.html
+echo "<h1>" >> Report.html
+echo "Failed Tests" >> Report.html
+echo "</h1>" >> Report.html
+
+for f in Report.txt;
+do
+	echo "$f" >> Report.html
+	echo "</br>" >> Report.html
+done
+
+echo "</body>" >> Report.html
+echo "</html>" >> Report.html
 
 #launch reports.html 
 x-www-browser reports.html
