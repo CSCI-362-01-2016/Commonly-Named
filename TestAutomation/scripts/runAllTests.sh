1617de122714a7c8e706cb7480e7a923ae518062
James Rhoten <rhotenm@g.cofc.edu>
2016-11-16 18:01:47 -0500

    Update runAllTests.sh

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-#Delete Report.txt & Report.html
-rm Report.txt
+#Delete Report.html
 rm Report.html
 
-#Compile java code
-javac MainDriver.java
-javac ContainsDriver.java
-javac StringUtil.java
-
 #run all test cases
-java MainDriver
+#
+#for each .txt file in testCases..
+FILES="/testCases/*.txt"
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # Reads each line in file to a cell in an array called lines
+  # ${lines[0]} = testCaseNumber
+  # ${lines[1]} = requirement being tested
+  # ${lines[2]} = class.method
+  # ${lines[3]} = driver.java
+  # ${lines[4]} = oracle
+  # ${lines[5]} = inputs
+  IFS=$'\n' read -d '' -r -a lines < $f
+  cat $f
+  #compile ${lines[3]}
+  #run driver with arguments in ${lines[5]}
+  #store result in ${lines[6]}
+  #compare ${lines[4]} to ${lines[6]}
+  #store report in ${lines[7]}
+  #format row of table
+  
+done
 
 #compare test output to oracles and create report
 ./compare.py Results.txt Oracle.txt Report.txt
