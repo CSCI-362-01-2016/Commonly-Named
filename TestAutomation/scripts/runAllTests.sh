638b722c5c8b81cd06c3655f97c71706449e3cde
James Rhoten <rhotenm@g.cofc.edu>
2016-11-21 17:32:23 -0500

    Update runAllTests.sh

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -3,14 +3,38 @@
 #Delete Report.html
 rm /temp/Report.html
 
+#create Reports.html with header
+{
+  echo "<!DOCTYPE html>"
+  echo "<html>" 
+  echo "<body>" 
+  echo "<h1>" 
+  echo "TESTING REPORT" 
+  echo "</h1>"
+  echo "<table>"
+  echo "<tr>"
+  echo "  <th>Test Case Number</th>"
+  echo "  <th>Requirment Tested</th>"
+  echo "  <th>class.method tested</th>"
+  echo "  <th>Driver Source</th>"
+  echo "  <th>Oracle</th>"
+  echo "  <th>Method Inputs</th>" 
+  echo "  <th>Method Output</th>"
+  echo "  <th>Test Result</th>"
+  echo "</tr>"
+} >> /temp/Report.html  
+
+
 #run all test cases
 #
 #for each .txt file in testCases..
 FILES="/testCases/*.txt"
 for f in $FILES
 do
-  SRC="/testCaseExecutables/"
+  SRC="/testCaseExecutables/"  #dir containing driver.java files
+  COLOR="green"                #default color of test result indicating PASS
   echo "Processing $f file..."
+  
   # Reads each line in file to a cell in an array called lines
   # ${lines[0]} = testCaseNumber
   # ${lines[1]} = requirement being tested
@@ -19,20 +43,25 @@ do
   # ${lines[4]} = oracle
   # ${lines[5]} = inputs
   IFS=$'\n' read -d '' -r -a lines < "$f"
+  
   #create source path from SRC variable and line 3
   SRC+="${lines[3]}"
   echo "$SRC"
+  
   #compile /testCaseExecutables/${lines[3]}
   BUILDCMD="javac $SRC"
   echo "$BUILDCMD"
   eval "$BUILDCMD"  #creates driver.class file in /testCaseExecutables/
+  
   #DRIVER="/testCaseExecutables/" + ${lines[3]} - ".java"
   DRIVER="${SRC%.*}"
   echo "$DRIVER"
+  
   #run driver with arguments in ${lines[5]} and  store result in ${lines[6]}
   RUNCMD="java $DRIVER ${lines[5]}"
   echo "$RUNCMD"
   lines[6]=$(eval "$RUNCMD")
+  
   #compare ${lines[4]} to ${lines[6]} and store report in ${lines[7]}
   if [ "${lines[4]}" == "${lines[6]}" ]; then   
   lines[7]="PASS"
@@ -40,30 +69,52 @@ do
   lines[7]="FAIL"
   fi
   
+  #Change $COLOR to red if test failed
+  if [ "${lines[7]}" == "FAIL" ]; then
+    COLOR="red"
+  fi  
   
-  #format row of table
+  #format row of table for this test case
+  {
+    echo "<tr>"
+    echo "  <th>"
+    echo ${lines[0]}
+    echo " </th>"
+    echo "  <th>"
+    echo ${lines[1]}
+    echo " </th>"
+    echo "  <th>"
+    echo ${lines[2]}
+    echo " </th>"
+    echo "  <th>"
+    echo ${lines[3]}
+    echo " </th>"
+    echo "  <th>"
+    echo ${lines[4]}
+    echo " </th>"
+    echo "  <th>"
+    echo ${lines[5]}
+    echo " </th>" 
+    echo "  <th>"
+    echo ${lines[6]}
+    echo " </th>"
+    echo "  <th>"
+    echo " <font color=$COLOR>" #color codes test result for quick, easy assessment
+    echo ${lines[7]}            #green indicates PASS; red indicates FAIL
+    echo " </font>"
+    echo " </th>"
+    echo "</tr>"
+} >> /temp/Reports.html
   
 done
 
 
-#change report.txt to report.html
-echo "<!DOCTYPE html>" > Report.html
-echo "<html>" >> Report.html
-echo "<body>" >> Report.html
-echo "<h1>" >> Report.html
-echo "Failed Tests" >> Report.html
-echo "</h1>" >> Report.html
-
-for f in Report.txt;
-do
-	echo "$f" >> Report.html
-	echo "</br>" >> Report.html
-done
-
-echo "</br>" >> Report.html
-echo "ALL OTHER TESTS HAVE PASSED" >> Report.html
-echo "</body>" >> Report.html
-echo "</html>" >> Report.html
+#Reports.html footer
+{
+  echo "</table>"
+  echo "</body>"
+  echo "</html>"
+} >> /temp/Reports.html
 
 #launch Report.html 
 x-www-browser Report.html
